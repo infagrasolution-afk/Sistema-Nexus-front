c497e9208f4c7e16b0eabc9321144b61d75d78c3
infagrasolution-afk <infagrasolution@gmail.com>
2026-05-31 20:13:21 -0400

    fix: eliminar importación no utilizada de DialogContent para solucionar fallo de compilacion en Render

--- src/components/SystemWizard.tsx
+++ src/components/SystemWizard.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { 
-  Dialog, DialogContent, Box, Typography, Button, IconButton, 
+  Dialog, Box, Typography, Button, IconButton, 
   Checkbox, FormControlLabel, MobileStepper, useTheme
 } from '@mui/material';
 import { 
